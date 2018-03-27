cf249edb132257cf84b26c3d9bfe4fcc8463df97
pritam-fourkites <pritam.gembali@fourkites.com>
2018-03-27 12:58:08 +0530

    first commit

---
+++ app/models/movie.rb
@@ -0,0 +1,8 @@
+class Movie < ApplicationRecord
+	searchkick
+	belongs_to :user
+	has_many :reviews
+
+	has_attached_file :image, styles: { medium: "400x600#"}
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+end
